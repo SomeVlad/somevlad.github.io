17766b3441ac5ed2a309654b66c806b663fa9492
Vlad Samoylov <samoylov.dev@gmail.com>
2019-04-13 22:12:41 +0300

    post rendering shenanigans

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import styles from './style.module.css'
 
 function Post({ content, title }) {
-    const html = content.rendered
     const heading = title.rendered
+    const __html = content.rendered
     return (
         <article key={heading} className={styles.root}>
             <h1>{heading}</h1>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <div dangerouslySetInnerHTML={{ __html }} />
         </article>
     )
 }
