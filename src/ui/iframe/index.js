91c3136400b38a8f9f8ae40b9c615c74832b18e4
Vlad Samoylov <samoylov.dev@gmail.com>
2019-04-20 00:55:54 +0300

    iframe with youtube -> link with poster

--- src/ui/iframe/index.js
+++ src/ui/iframe/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import styles from './style.module.css'
+import classNames from 'classnames/bind'
+import {
+    getYoutubeVideoIdFromString,
+    getYoutubeThumbnailImageUrl,
+    getYoutubeVideoLink
+} from 'helpers'
+import { A, Img } from 'ui'
 
-export const Iframe = ({ src, title }) => (
-    <div className={styles['iframe-wrap']}>
-        <iframe className={styles.iframe}
-                src={src}
-                title={title} />
-    </div>
+const cx = classNames.bind(styles)
+const youtubePreviewLinkClassNames = cx('iframe', 'video-link')
+
+const YoutubeVideoPreview = ({ id }) => (
+    <A
+        className={youtubePreviewLinkClassNames}
+        href={getYoutubeVideoLink(id)}>
+        <Img src={getYoutubeThumbnailImageUrl(id)}
+             className={cx('iframe-image')}
+             alt='youtube video'
+             title='youtube video' />
+    </A>
 )
+
+export const Iframe = props => {
+    const { src, title } = props
+    const youtubeVideoId = getYoutubeVideoIdFromString(src)
+    return (
+        <div className={cx('iframe-wrap')}>
+            {youtubeVideoId ? (
+                <YoutubeVideoPreview id={youtubeVideoId} />
+            ) : (
+                <iframe className={cx('iframe')}
+                        src={src}
+                        title={title} />
+            )}
+        </div>
+    )
+}
--- src/ui/img/index.js
+++ src/ui/img/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import classNames from 'classnames'
 import styles from './style.module.css'
 
-export const Img = ({ src, alt, title, sizes, srcset }) => (
-    <img
-        className={styles.img}
-        src={src}
-        alt={alt}
-        title={title}
-        sizes={sizes}
-        srcSet={srcset} />
-)
+export const Img = ({ src, alt, title, sizes, srcset, className }) => {
+    const imageClassNames = classNames(className, styles.img)
+    return (
+        <img
+            className={imageClassNames}
+            src={src}
+            alt={alt}
+            title={title}
+            sizes={sizes}
+            srcSet={srcset} />
+    )
+}
