1cee2f63a2c4df33f0524a902b98976c57bd7cec
Vlad Samoylov <samoylov.dev@gmail.com>
2019-04-21 18:52:47 +0300

    fix rel=false

--- src/ui/a/index.js
+++ src/ui/a/index.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 export const A = ({ href, children, className }) => {
     const isInternal = new URL(href).hostname === window.location.hostname
     const target = isInternal ? '_self' : '_blank'
-    const rel = !isInternal && 'noreferrer noopener'
+    const rel = isInternal ? null : 'noreferrer noopener'
     return (
         <Fragment>
             <a
