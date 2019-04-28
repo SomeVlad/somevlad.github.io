e71444fbde7048876012341f627f1569cc0ebb05
Vlad Samoylov <samoylov.dev@gmail.com>
2019-04-28 18:03:11 +0300

    fetchn't posts if in store already

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,19 +5,28 @@ export const POSTS_GET_SUCCESS = 'POSTS_GET_SUCCESS'
 export const POSTS_GET_FAILURE = 'POSTS_GET_FAILURE'
 
 export const getPosts = () => {
-    return dispatch => {
+    return (dispatch, getState) => {
         dispatch({
             type: POSTS_GET
         })
 
-        return postsRequest()
-            .then(posts => dispatch({
-                type: POSTS_GET_SUCCESS,
-                payload: posts
-            }))
-            .catch(error => dispatch({
-                type: POSTS_GET_FAILURE,
-                payload: error
-            }))
+        const { posts } = getState()
+
+        if (posts.length === 0) {
+            return postsRequest()
+                .then(posts => dispatch({
+                    type: POSTS_GET_SUCCESS,
+                    payload: posts
+                }))
+                .catch(error => dispatch({
+                    type: POSTS_GET_FAILURE,
+                    payload: error
+                }))
+        }
+
+        return dispatch({
+            type: POSTS_GET_SUCCESS,
+            payload: posts
+        })
     }
 }
