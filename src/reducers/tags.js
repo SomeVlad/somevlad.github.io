caa3a108319582b87e17c9717fbb664e982a551e
Vlad Samoylov <samoylov.dev@gmail.com>
2019-05-03 20:10:05 +0300

    add tags reducer

---
+++ src/reducers/tags.js
@@ -0,0 +1,13 @@
+import {
+    TAGS_GET_SUCCESS
+} from 'actions'
+
+export default (state = [], { type, payload }) => {
+    switch (type) {
+        case TAGS_GET_SUCCESS:
+            return payload
+
+        default:
+            return state
+    }
+}
