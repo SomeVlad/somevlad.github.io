f5cca7ccb9953ac522b91228935167032daa019f
Vlad Samoylov <samoylov.dev@gmail.com>
2019-05-13 14:42:31 +0300

    fix memory leak issue in logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import styles from './style.module.css'
 
+const LOGO_RERENDER_INTERVAL = 5000
+
 const getAlternateLetters = () => `0${(Math.floor(Math.random() * 17) + 1)}`.slice(-2)
 const getTextTransform = () => ['capitalize', 'lowercase', 'uppercase'][Math.floor(Math.random() * 3)]
 
 export class Logo extends Component {
     state = {
         fontFeatureSettings: `'ss${getAlternateLetters()}' 1`,
-        textTransform: getTextTransform()
+        textTransform: getTextTransform(),
+        interval: null
     }
 
     componentDidMount() {
-        setInterval(() => this.setState({
-            fontFeatureSettings: `'ss${getAlternateLetters()}' 1`,
-            textTransform: getTextTransform()
-        }), 5000)
+        this.setState({
+            interval: setInterval(() => this.setState({
+                fontFeatureSettings: `'ss${getAlternateLetters()}' 1`,
+                textTransform: getTextTransform()
+            }), LOGO_RERENDER_INTERVAL)
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval)
     }
 
     render() {
