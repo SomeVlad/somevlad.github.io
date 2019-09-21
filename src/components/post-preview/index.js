abede370e7296ee19f8b09d45d3bbd1ad5f77534
Vlad Samoylov <samoylov.dev@gmail.com>
2019-09-21 23:17:11 +0300

    - add dark theme

--- src/components/post-preview/index.js
+++ src/components/post-preview/index.js
@@ -1,46 +1,4 @@
-import React, { Component } from 'react'
-import * as PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import { H1 } from 'ui'
-import {
-    renderNodeAsComponent,
-} from 'helpers'
-import styles from './style.module.css'
+import React from 'react'
+import { Post } from 'components'
 
-const PostTitle = ({ heading, url }) => (
-    <Link to={url}>
-        <H1>{heading}</H1>
-    </Link>
-)
-
-PostTitle.propTypes = {
-    heading: PropTypes.string,
-    url: PropTypes.string,
-}
-
-class PostPreview extends Component {
-    static propTypes = {
-        heading: PropTypes.string,
-        contents: PropTypes.array,
-        slug: PropTypes.string,
-    }
-
-    render() {
-        const { heading, contents, slug } = this.props
-
-        if (!contents) {
-            return 'loading...'
-        }
-
-        return (
-            <article key={heading} className={styles.article}>
-                <PostTitle
-                    heading={heading}
-                    url={`/posts/${slug}`} />
-                {contents.map(renderNodeAsComponent)}
-            </article>
-        )
-    }
-}
-
-export { PostPreview }
+export const PostPreview = props => <Post {...props} preview />
