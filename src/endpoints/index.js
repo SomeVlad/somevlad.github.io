3202d81fb85b3c1b9f971f8d2b186257d9cbbb66
Vlad Samoylov <samoylov.dev@gmail.com>
2019-09-22 21:35:11 +0300

    remove cname

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://blog.vladsamoylov.com'
+const baseUrl = 'https://vladsamoylov.com'
 
 export const Endpoints = {
     posts: baseUrl + '/wp-json/wp/v2/posts?per_page=100',
--- src/requests/index.js
+++ src/requests/index.js
@@ -1,13 +1,13 @@
 import { Endpoints } from 'endpoints'
 
 export const tagsRequest = async () => {
-    const result = await fetch(Endpoints.tags, { mode: 'no-cors' })
+    const result = await fetch(Endpoints.tags)
 
     return await result.json()
 }
 
 export const postsRequest = async () => {
-    const result = await fetch(Endpoints.posts, { mode: 'no-cors' })
+    const result = await fetch(Endpoints.posts)
 
     return await result.json()
 }
